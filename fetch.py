58cd554ad76ea7636494aea1f13b31fd4460d681
Sean MacAvaney <sean.macavaney@gmail.com>
2022-01-14 10:18:01 +0000

    added vaswani files

--- fetch.py
+++ fetch.py
@@ -9,31 +9,35 @@ import datetime
 parser = argparse.ArgumentParser()
 parser.add_argument('download_id')
 parser.add_argument('--list', action='store_true')
+parser.add_argument('--expected_md5')
 args = parser.parse_args()
 
-downloads = json.loads(pkgutil.get_data('ir_datasets', 'etc/downloads.json'))
+if args.download_id.startswith('http://') or args.download_id.startswith('https://'):
+  download = {'url': args.download_id, 'expected_md5': args.expected_md5}
+else:
+  downloads = json.loads(pkgutil.get_data('ir_datasets', 'etc/downloads.json'))
 
-def iter_downloads(data, prefix=''):
-  if 'url' in data and 'expected_md5' in data:
-    yield prefix, data
-  elif 'instructions' in data:
-    pass
-  else:
-    for key in data.keys():
-      yield from iter_downloads(data[key], prefix=f'{prefix}/{key}' if prefix else key)
+  def iter_downloads(data, prefix=''):
+    if 'url' in data and 'expected_md5' in data:
+      yield prefix, data
+    elif 'instructions' in data:
+      pass
+    else:
+      for key in data.keys():
+        yield from iter_downloads(data[key], prefix=f'{prefix}/{key}' if prefix else key)
 
-download = None
-for dlid, dlc in iter_downloads(downloads):
-  if args.list:
-    print(f'{dlid}\t{dlc["url"]}\t{dlc["expected_md5"]}')
-  if dlid == args.download_id:
-    download = dlc
-    if not args.list:
-      break
+  download = None
+  for dlid, dlc in iter_downloads(downloads):
+    if args.list:
+      print(f'{dlid}\t{dlc["url"]}\t{dlc["expected_md5"]}')
+    if dlid == args.download_id:
+      download = dlc
+      if not args.list:
+        break
 
-if not download:
-  print(f'could not find download {args.download_id}')
-  sys.exit(-1)
+  if not download:
+    print(f'could not find download {args.download_id}')
+    sys.exit(-1)
 
 dlc = ir_datasets.util.Download([ir_datasets.util.RequestsDownload(download['url'])], expected_md5=download['expected_md5'], stream=True)
 size = 0
